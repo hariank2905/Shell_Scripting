0e896b6db5e5bf8ffce809d2f334b2ab57f1011f
harikishore <hankiab05@gmail.com>
2023-08-17 11:37:24 +0530

    array practice

--- 05-variables.sh
+++ 05-variables.sh
@@ -3,5 +3,5 @@
 #here we can use array type which can store multiple values
 PERSONS=('Hari', 'Ankitha', 'Raji', 'Konda')
 
-    echo "Hello guys:$PERSONS[0:2]"
-    echo "Hello guys:$PERSONS[@]"
+    echo "Hello guys:${PERSONS[0:2]}"
+    echo "Hello guys:${PERSONS[@]}"
