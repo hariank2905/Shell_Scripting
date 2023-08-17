84af2274669877251fe2e886e12066607bf46e59
harikishore <hankiab05@gmail.com>
2023-08-17 11:34:07 +0530

    array practice

---
+++ 05-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#here we can use array type which can store multiple values
+PERSONS=('Hari', 'Ankitha', 'Raji', 'Konda')
+
+    echo (Hello guys:$PERSONS[0:2])
+    echo (Hello guys:$PERSONS[@])
