c644a742401980791f3952f7194763db4b149738
harikishore <hankiab05@gmail.com>
2023-08-17 11:48:51 +0530

    if condition numbers

--- 07-conditons.sh
+++ 07-conditons.sh
@@ -4,10 +4,18 @@
 
 
 NUMBER=$1
-
+: '
 if [$NUMBER -gt 100] # -gt means greater than , -ge means greaterthan 0r equal to....
 then
     echo "The given number $NUMBER is greater than the 100"
 else
     echo "The number is not greater than 100"
+fi 
+'
+
+if [ $NUMBER -gt 10 ]
+then
+    echo "$NUMBER is greater than 10"
+else
+    echo "$NUMBER is not greater than 10"
 fi
