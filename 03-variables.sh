bcefc879605e46cb1ea512068354ab8319c96263
harikishore <hankiab05@gmail.com>
2023-08-17 11:24:52 +0530

    User Credentials

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# this is the comment, we need to develop a program that can do addition of 2 numbers
 NUM1=$1
 NUM2=$2
+
+# I need to run a command inside shell to add these 2 numbers
 SUM=$((NUM1+NUM2))
 
-    echo "here is the addition of two given numbers: $SUM"
+    echo "here is the addition of two given numbers: $SUM "
---
+++ 04-variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "please enter your username"
+
+read -s USERNAME #here where the value of user can enter username
+
+echo "Username is : $USERNAME"
+
+echo "please enter your password"
+
+read -s PASSWORD #here where he can enter the password
+
+echo " password is : $PASSWORD" 
+
